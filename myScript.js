f768742facae528752f9c70e2665128bc22d5ae9
mardukkkurios <arcis_scio@hotmail.com>
2013-10-08 09:18:19 -0500

    intento fallido de agregar el resize

--- myScript.js
+++ myScript.js
@@ -22,31 +22,33 @@ window.onload=function(){
 
 //**********DRAG EVENTS**************************************//
 var dragSrcEl = null;
+var dragOrigin = 0;
 function handleDragStart(e) {
   this.style.opacity = '0.4';  // this / e.target is the source node.
   dragSrcEl = this;
 
-  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.effectAllowed = 'copy';
   e.dataTransfer.setData('text/html', this.innerHTML);
 }
 
 function handleDragOver(e) {
-  if (e.preventDefault) {
-    e.preventDefault(); // Necessary. Allows us to drop.
-  }
-
-  e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
-
+  if(dragSrcEl!=null){
+	  if (e.preventDefault) {
+		e.preventDefault(); // Necessary. Allows us to drop.
+	  }
+	  e.dataTransfer.dropEffect = 'copy';  // See the section on the DataTransfer object.
+	  this.classList.add('over');
+  } 
   return false;
 }
 
 function handleDragEnter(e) {
   // this / e.target is the current hover target.
-  this.classList.add('over');
+  if(dragSrcEl!=null){this.classList.add('over');}
 }
 
 function handleDragLeave(e) {
-  this.classList.remove('over');  // this / e.target is previous target element.
+  if(dragSrcEl!=null){this.classList.remove('over');}  // this / e.target is previous target element.
 }
 
 function handleDrop(e) {
@@ -56,24 +58,95 @@ function handleDrop(e) {
 		e.stopPropagation(); // Stops some browsers from redirecting.
 	}
 	
-
+	if(dragSrcEl!=null)
+	{
 		// Set the source column's HTML to the HTML of the column we dropped on.
 		var temp = document.importNode(dragSrcEl, true);
 		//e.dataTransfer.getData('text/html');
+		temp.addEventListener('dragstart', handleDragStartForResize, false);
 		temp.addEventListener('dragenter', handleDragEnter, false);
 		temp.addEventListener('dragover', handleDragOver, false);
 		temp.addEventListener('dragleave', handleDragLeave, false);
 		temp.addEventListener('drop', handleDrop, false);
-		temp.addEventListener('dragend', handleDragEnd, false);
+		temp.addEventListener('dragend', handleDragEndForResize, false);
 		temp.style.opacity = "1.0";
-		temp.draggable = false;
+		temp.draggable = true;
 		this.appendChild(temp);
 		
-	
+		dragSrcEl=null;
+		e.dataTransfer.clearData();
+	}
 	return false;
 }
 
 
-function handleDragEnd(e) {
-  this.style.opacity = '1.0';  // this / e.target is the source node.
+function handleDragEnd(e){
+	this.style.opacity = '1.0';  // this / e.target is the source node.
+}
+
+////********Resize********/////////
+function handleDragEndForResize(e) {
+	var newPaddingX;
+	var newPaddingY;
+	var oldPaddingLeft   = isNaN(parseInt(this.style.paddingLeft))?0:parseInt(this.style.paddingLeft);
+	var oldPaddingRight  = isNaN(parseInt(this.style.paddingRight))?0:parseInt(this.style.paddingRight);
+	var oldPaddingTop    = isNaN(parseInt(this.style.paddingTop))?0:parseInt(this.style.paddingTop);
+	var oldPaddingBottom = isNaN(parseInt(this.style.paddingBottom))?0:parseInt(this.style.paddingBottom);
+	//alert(parseInt(e.offsetX));
+	switch(dragOrigin)
+	{
+	case 1: newPaddingX=oldPaddingLeft-parseInt(e.offsetX)+"px";
+			this.style.paddingLeft=parseInt(newPaddingX)<0?"0px":newPaddingX;
+			newPaddingY=oldPaddingTop-parseInt(e.offsetY)+"px";
+			this.style.paddingTop=parseInt(newPaddingY)<0?"0px":newPaddingY;
+			break;
+	case 2:	newPaddingY=oldPaddingTop-parseInt(e.offsetY)+"px";
+			this.style.paddingTop=parseInt(newPaddingY)<0?"0px":newPaddingY;
+			break;
+	case 3:	newPaddingX=oldPaddingRight+(parseInt(e.offsetX)-parseInt(this.offsetWidth))+"px";
+			this.style.paddingRight=parseInt(newPaddingX)<0?"0px":newPaddingX;
+			newPaddingY=oldPaddingTop-parseInt(e.offsetY)+"px";
+			this.style.paddingTop=parseInt(newPaddingY)<0?"0px":newPaddingY;
+			break;
+	case 4:	newPaddingX=oldPaddingLeft-parseInt(e.offsetX)+"px";
+			this.style.paddingLeft=parseInt(newPaddingX)<0?"0px":newPaddingX;
+			break;
+	case 5:	break;
+	case 6:	newPaddingX=oldPaddingRight+(parseInt(e.offsetX)-parseInt(this.offsetWidth))+"px";
+			this.style.paddingRight=parseInt(newPaddingX)<0?"0px":newPaddingX;
+			break;
+	case 7:	newPaddingX=oldPaddingLeft-parseInt(e.offsetX)+"px";
+			this.style.paddingLeft=parseInt(newPaddingX)<0?"0px":newPaddingX;
+			newPaddingY=oldPaddingBottom+(parseInt(e.offsetY)-parseInt(this.offsetHeight))+"px";
+			this.style.paddingBottom=parseInt(newPaddingY)<0?"0px":newPaddingY;
+			break;
+	case 8: newPaddingY=oldPaddingBottom+(parseInt(e.offsetY)-parseInt(this.offsetHeight))+"px";
+			this.style.paddingBottom=parseInt(newPaddingY)<0?"0px":newPaddingY;
+			break;
+	case 9:	newPaddingX=oldPaddingRight+(parseInt(e.offsetX)-parseInt(this.offsetWidth))+"px";
+			this.style.paddingRight=parseInt(newPaddingX)<0?"0px":newPaddingX;
+			newPaddingY=oldPaddingBottom+(parseInt(e.offsetY)-parseInt(this.offsetHeight))+"px";
+			this.style.paddingBottom=parseInt(newPaddingY)<0?"0px":newPaddingY;
+			break;
+	}
+	return false;
+}
+
+function handleDragStartForResize(e){
+	dragOrigin=9;
+	if(e.offsetY<this.offsetHeight-20){dragOrigin=6;}
+	if(e.offsetY<20){dragOrigin=3;}
+	if(e.offsetX<this.offsetWidth-20){ 
+		dragOrigin=8;
+		if(e.offsetY<this.offsetHeight-20){dragOrigin=5;}
+		if(e.offsetY<20){dragOrigin=2;}
+	}
+	if(e.offsetX<20){
+		dragOrigin=7;
+		if(e.offsetY<this.offsetHeight-20){dragOrigin=4;}
+		if(e.offsetY<20){dragOrigin=1;}
+	}
+	//e.dataTransfer.effectAllowed='none';
+	this.style.pointerEvents='none';
+	return false;
 }
