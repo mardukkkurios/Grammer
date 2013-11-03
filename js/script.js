73e42c29d5a40198debb5cdeacb3305fd2dd3e69
Eduardo de Jesus Aguilar Plascencia <arcis_scio@hotmail.com>
2013-11-02 23:18:22 -0600

    :D
    Un monton de cambios desde la ultima vez

--- js/script.js
+++ js/script.js
@@ -6,8 +6,6 @@ var clipboard= new Array();
 
 //**********ONLOAD FUNCTION**********************************//
 window.onload=function(){			
-	//DEBUG
-	debug = document.getElementById("debug");
 	//PALETA
 	objetosPaleta = document.querySelectorAll('#interfazDeObjetos *');
 	[].forEach.call(objetosPaleta, function(obj) {
@@ -22,6 +20,7 @@ window.onload=function(){
 	temp.addEventListener('dragleave', handleDragLeave, false);
 	temp.addEventListener('drop', handleDrop, false);
 	temp.addEventListener('dragend', handleDragEnd, false);
+	temp.addEventListener('click', handleClickForCleanSelection, false);
 	
 	temp=document.getElementsByTagName("body");
 	temp[0].addEventListener("keydown",handleKeyDown,false);
@@ -34,58 +33,51 @@ window.onload=function(){
 };
 
 //**********DRAG EVENTS**************************************//
-function handleDragStart(e) {
+function handleDragStart(e){
   this.style.opacity = '0.4';  // this / e.target is the source node.
   dragSrcEl = this;
+  dragOrigin=5;
 
   e.dataTransfer.effectAllowed = 'copy';
   e.dataTransfer.setData('text/html', this.innerHTML);
 }
 
-function handleDragOver(e) {
-	if (e.preventDefault) {
-		e.preventDefault(); // Necessary. Allows us to drop.
-	}
-	if(dragSrcEl!=null){
-		this.classList.add('over');
-	} 
+function handleDragOver(e){
+	e.preventDefault(); // Necessary. Allows us to drop.
+	if(dragSrcEl!=null) this.classList.add('over');
 	return false;
 }
 
-function handleDragEnter(e) {
-  // this / e.target is the current hover target.
-  if(dragSrcEl!=null){this.classList.add('over');}
+function handleDragEnter(e){
+  if(dragSrcEl!=null) this.classList.add('over');// this / e.target is the current hover target.
 }
 
-function handleDragLeave(e) {
-  if(dragSrcEl!=null){this.classList.remove('over');}  // this / e.target is previous target element.
+function handleDragLeave(e){
+  if(dragSrcEl!=null) this.classList.remove('over');  // this / e.target is previous target element.
 }
 
-function handleDrop(e) {
-  // this / e.target is current target element.
+function handleDrop(e){
+	e.stopPropagation(); // Stops some browsers from redirecting.
+	//Removing over of the all elements
 	var t=document.getElementsByClassName('over');
-	for(var i=0;i<t.length;i++){
-		t[i].classList.remove('over');
-	}
+	for(var i=0;i<t.length;i++) t[i].classList.remove('over');
 	
-	if (e.stopPropagation) {
-		e.stopPropagation(); // Stops some browsers from redirecting.
-	}
-	
-	if(dragSrcEl!=null)
-	{//falta seleccion multiple
-		var temp;
+	if(dragSrcEl!=null){//falta seleccion multiple
+		var temp=new Array();
 		if(e.dataTransfer.effectAllowed=='copy'){
-			// Set the source column's HTML to the HTML of the column we dropped on.
-			temp = document.importNode(dragSrcEl, true);
-			addEvents(temp);
+			temp.push(document.importNode(dragSrcEl,true));
+			addEvents(temp[0]);
 		}
-		if(e.dataTransfer.effectAllowed=='move')temp=dragSrcEl;
-		temp.style.opacity = "1.0";
-		this.appendChild(temp);
+		if(e.dataTransfer.effectAllowed=='move'){
+			temp=seleccion;
+			dragOrigin=0;
+		}
+		for(var k in temp){
+			this.appendChild(temp[k]);
+			temp[k].style.opacity="1.0";
+		}
 
 		dragSrcEl=null;
-		dragOrigin=0;
 		e.dataTransfer.clearData();
 	}
 	return false;
@@ -95,16 +87,15 @@ function handleDragEnd(e){
 	this.style.opacity = '1.0';  // this / e.target is the source node.
 }
 
-////********Resize and Move********/////////
-function handleDragEndForResize(e) {
+////********Resize and Move********////
+function handleDragEndForResize(e){
 	var newPaddingX;
 	var newPaddingY;
 	var oldPaddingLeft   = isNaN(parseInt(this.style.paddingLeft))?0:parseInt(this.style.paddingLeft);
 	var oldPaddingRight  = isNaN(parseInt(this.style.paddingRight))?0:parseInt(this.style.paddingRight);
 	var oldPaddingTop    = isNaN(parseInt(this.style.paddingTop))?0:parseInt(this.style.paddingTop);
 	var oldPaddingBottom = isNaN(parseInt(this.style.paddingBottom))?0:parseInt(this.style.paddingBottom);
-	switch(dragOrigin)
-	{
+	switch(dragOrigin){
 		case 1: newPaddingX=oldPaddingLeft-parseInt(e.offsetX)+"px";
 				this.style.paddingLeft=parseInt(newPaddingX)<0?"0px":newPaddingX;
 				newPaddingY=oldPaddingTop-parseInt(e.offsetY)+"px";
@@ -145,106 +136,157 @@ function handleDragEndForResize(e) {
 }
 
 function handleDragStartForResize(e){
-	if(this==e.target){
-		dragOrigin=9;
-		if(e.offsetY<this.offsetHeight-10){dragOrigin=6;}
-		if(e.offsetY<10){dragOrigin=3;}
-		if(e.offsetX<this.offsetWidth-10){ 
-			dragOrigin=8;
-			if(e.offsetY<this.offsetHeight-10){
-				dragOrigin=5;
-				moving=true;
-				this.style.opacity = '0.4';  // this / e.target is the source node.
-				dragSrcEl = this;
-				e.dataTransfer.effectAllowed = 'move';
-				e.dataTransfer.setData('text/html', this.innerHTML);
-			}
-			if(e.offsetY<10){
-				dragSrcEl=null;
-				dragOrigin=2;
-				e.dataTransfer.clearData();
-			}
-		}
-		if(e.offsetX<10){
-			dragSrcEl=null;
-			dragOrigin=7;
-			e.dataTransfer.clearData();
-			if(e.offsetY<this.offsetHeight-10){dragOrigin=4;}
-			if(e.offsetY<10){dragOrigin=1;}
+	dragOrigin=9;
+	if(e.offsetY<this.offsetHeight-10) dragOrigin=6;
+	if(e.offsetY<10) dragOrigin=3;
+	if(e.offsetX<this.offsetWidth-10){ 
+		dragOrigin=8;
+		if(e.offsetY<this.offsetHeight-10) dragOrigin=5;
+		if(e.offsetY<10) dragOrigin=2;
+	}
+	if(e.offsetX<10){
+		dragOrigin=7;
+		if(e.offsetY<this.offsetHeight-10) dragOrigin=4;
+		if(e.offsetY<10) dragOrigin=1;
+	}
+	if(dragOrigin==5){
+		this.style.opacity = '0.4'; 
+		dragSrcEl = this;
+		e.dataTransfer.effectAllowed = 'move';
+		e.dataTransfer.setData('text/html', this.innerHTML);
+		if(seleccion.indexOf(this)==-1){//Esto es para poner en objeto en eleccion al hacer move
+			var temp=document.getElementsByTagName("*");//si se hace click sin ctrl entonces se limpia toda la seleccion
+			//y a todos los elementos se les retira la clase "seleccionado"
+			for(var i=0;i<temp.length;i++) temp[i].classList.remove("seleccionado");
+			seleccion=new Array();//luego se agrega el elemento clickeado a la selecion y se le agrega la clase
+			seleccion.push(this);
+			this.classList.add("seleccionado");
 		}
 	}
+	e.stopPropagation();
 	return false;
 }
 
-function handleDropForMove(e) {
-  // this / e.target is current target element.
-	var t=document.getElementsByClassName('over');
-	for(var i=0;i<t.length;i++){
-		t[i].classList.remove('over');
-	}
-	
-	if (e.stopPropagation) {
-		e.stopPropagation(); // Stops some browsers from redirecting.
-	}
-	
-	var temp=null;
-	if(e.dataTransfer.effectAllowed=='copy'){
-		temp=document.importNode(dragSrcEl,true);
-		addEvents(temp);
-	}
-	if(e.dataTransfer.effectAllowed=='move'){
-		//esto tiene que cambiar para multiseleccion
-		temp=dragSrcEl;
-	}
-	temp.style.opacity = "1.0";
-	if(dragSrcEl!=null)
-	{
-		if(e.offsetY < this.offsetHeight/5){
-			this.parentNode.insertBefore(temp,this);
-		}else{ 
-			if(e.offsetY > this.offsetHeight*4/5){
-				insertAfter(temp,this);
-			}else{
-				if(e.offsetX < this.offsetWidth/5){
-					this.parentNode.insertBefore(temp,this);
-				}else{
-					if(e.offsetX > this.offsetWidth*4/5){
-						insertAfter(temp,this);
-					}else{
-						this.appendChild(temp);
+function handleDropForMove(e){
+	if(dragOrigin==5){
+		e.stopPropagation();
+		var t=document.getElementsByClassName('over');
+		for(var i=0;i<t.length;i++) t[i].classList.remove('over');
+		
+		var temp=new Array();
+		if(e.dataTransfer.effectAllowed=='copy'){
+			temp.push(document.importNode(dragSrcEl,true));
+			addEvents(temp[0]);
+		}
+		if(e.dataTransfer.effectAllowed=='move') temp=seleccion;
+
+		for(var k in temp){
+			if(e.offsetY < this.offsetHeight/5) this.parentNode.insertBefore(temp[k],this);
+			else{ 
+				if(e.offsetY > this.offsetHeight*4/5) insertAfter(temp[k],this);
+				else{
+					if(e.offsetX < this.offsetWidth/5) this.parentNode.insertBefore(temp[k],this);
+					else{
+						if(e.offsetX > this.offsetWidth*4/5) insertAfter(temp[k],this);
+						else this.appendChild(temp[k]);
 					}
 				}
 			}
+			temp[k].style.opacity = "1.0";
 		}
-
+		e.dataTransfer.clearData();
 		dragSrcEl=null;
 		dragOrigin=0;
-		e.dataTransfer.clearData();
 	}
 	return false;
 }
 
 ////********Selection*********////
-function handleMouseDown(e){
-	if (this==e.target){
-		if(e.ctrlKey==1){//revisamos si esta o no la tecla control presionada
-			if(seleccion.indexOf(this)==-1){//si lo esta revisamos si el elemento clickeado esta actualmente entre los elementos seleccionados
-				seleccion.push(this);    //si no lo esta se agrega a la seleccion y se le agrega la clase css "seleccionado"
-				this.classList.add("seleccionado");
-			}else{//si ya lo esta se saca de la sseleccion y se le quita la clase css
-				seleccion.splice(seleccion.indexOf(this),1);//recomendado http://www.w3schools.com/jsref/jsref_splice.asp
-				this.classList.remove("seleccionado");
-			}
-		}else{
-			var temp=document.getElementsByTagName("*");//si se hace click sin ctrl entonces se limpia toda la seleccion
-			for(var i=0;i<temp.length;i++){//y a todos los elementos se les retira la clase "seleccionado"
-				temp[i].classList.remove("seleccionado");
-			}
-			seleccion=new Array();//luego se agrega el elemento clickeado a la selecion y se le agrega la clase
-			seleccion.push(this);
+function handleClick(e){
+	if(e.ctrlKey==true){//revisamos si esta o no la tecla control presionada
+		if(seleccion.indexOf(this)==-1){//si lo esta revisamos si el elemento clickeado esta actualmente entre los elementos seleccionados
+			seleccion.push(this);    //si no lo esta se agrega a la seleccion y se le agrega la clase css "seleccionado"
 			this.classList.add("seleccionado");
-		}	
+		}else{//si ya lo esta se saca de la sseleccion y se le quita la clase css
+			seleccion.splice(seleccion.indexOf(this),1);//recomendado http://www.w3schools.com/jsref/jsref_splice.asp
+			this.classList.remove("seleccionado");
+		}
+	}else{
+		var temp=document.getElementsByTagName("*");//si se hace click sin ctrl entonces se limpia toda la seleccion
+		//y a todos los elementos se les retira la clase "seleccionado"
+		for(var i=0;i<temp.length;i++) temp[i].classList.remove("seleccionado");
+		seleccion=new Array();//luego se agrega el elemento clickeado a la selecion y se le agrega la clase
+		seleccion.push(this);
+		this.classList.add("seleccionado");
+	}		
+	e.stopPropagation();
+}
+
+function handleDoubleClick(e){
+	var tdesc;
+	if(e.ctrlKey==true){//revisamos si esta o no la tecla control presionada
+		if(seleccion.indexOf(this)==-1){//si lo esta revisamos si el elemento clickeado esta actualmente entre los elementos seleccionados
+			seleccion.push(this);    //si no lo esta se agrega a la seleccion y se le agrega la clase css "seleccionado"
+			this.classList.add("seleccionado");//y se agregan todos los desce
+			tdesc=this.querySelectorAll('*');
+			[].forEach.call(tdesc,function(obj){
+				if(seleccion.indexOf(obj)==-1){
+					seleccion.push(obj);
+					obj.classList.add("seleccionado");
+				}
+			});
+		}else{//si ya lo esta se saca de la seleccion y se le quita la clase css
+			seleccion.splice(seleccion.indexOf(this),1);//recomendado http://www.w3schools.com/jsref/jsref_splice.asp
+			this.classList.remove("seleccionado");
+			tdesc=this.querySelectorAll('*');
+			[].forEach.call(tdesc,function(obj){
+				if(seleccion.indexOf(obj)!=-1){
+					seleccion.splice(seleccion.indexOf(obj),1);
+					obj.classList.remove("seleccionado");
+				}
+			});
+		}
+	}else{
+		var temp=document.getElementsByTagName("*");//si se hace click sin ctrl entonces se limpia toda la seleccion
+		//y a todos los elementos se les retira la clase "seleccionado"
+		for(var i=0;i<temp.length;i++) temp[i].classList.remove("seleccionado");
+		seleccion=new Array();//luego se agrega el elemento clickeado a la selecion y se le agrega la clase
+		seleccion.push(this);
+		this.classList.add("seleccionado");
+		tdesc=this.querySelectorAll('*');
+		[].forEach.call(tdesc,function(obj){
+			seleccion.push(obj);
+			obj.classList.add("seleccionado");
+		});
 	}
+	e.stopPropagation();
+}
+
+function handleClickForCleanSelection(e){
+	if(e.ctrlKey==false){
+		var temp=document.getElementsByTagName("*");
+		for(var i=0;i<temp.length;i++) temp[i].classList.remove("seleccionado");
+		seleccion=new Array();
+	}
+}
+
+////********Mouse*********////
+function handleMouseMove(e){//Solo para q cambie el cursor
+	this.style.cursor="se-resize";
+	if(e.offsetY<this.offsetHeight-10) this.style.cursor="e-resize";
+	if(e.offsetY<10) this.style.cursor="ne-resize";
+	if(e.offsetX<this.offsetWidth-10){ 
+		this.style.cursor="s-resize";
+		if(e.offsetY<this.offsetHeight-10) this.style.cursor="move";
+		if(e.offsetY<10) this.style.cursor="n-resize";
+	}
+	if(e.offsetX<10){
+		this.style.cursor="sw-resize";
+		if(e.offsetY<this.offsetHeight-10) this.style.cursor="w-resize";
+		if(e.offsetY<10) this.style.cursor="nw-resize";
+	}
+	e.stopPropagation();
+	return false;
 }
 
 ////*******Delete, cut, copy and paste elements*******////
@@ -252,41 +294,34 @@ function handleKeyDown(e){
 	switch(e.keyCode){
 		//DEL
 		case 46:while(seleccion.length>0){
-					if(seleccion[0]==null||seleccion[0].parentNode==null){
-						seleccion.shift()
-					}else{
+					if(seleccion[0]==null||seleccion[0].parentNode==null) seleccion.shift();
+					else{
+						// while(seleccion[0].children.length>0) seleccion[0].parentNode.insertBefore(seleccion[0].children[0],seleccion[0]);
 						seleccion[0].parentNode.removeChild(seleccion[0]);
 					}
 				}
 				break;
-		//C
-		case 67:clipboard=new Array();
-				for(var i=0;i<seleccion.length;i++){
-					clipboard.push(seleccion[i].cloneNode(true));
+		//Ctrl+C
+		case 67:if(e.ctrlKey==true){
+					clipboard=new Array();
+					for(var i=0;i<seleccion.length;i++) clipboard.push(seleccion[i].cloneNode(true));
 				}
 				break;
-		//V
-		case 86:if(seleccion.length==0){
-					var iDE=document.getElementById("interfazDeEdicion");
-					var t;
-					var tdesc;
-					for(var i=0;i<clipboard.length;i++){
-						t=clipboard[i].cloneNode(true);
-						addEvents(t);
-						t.classList.remove("seleccionado");
-						tdesc=t.querySelectorAll("*");
-						[].forEach.call(tdesc,function(obj){
-								addEvents(obj);
-								obj.classList.remove("seleccionado");
-							});
-						iDE.appendChild(t);
-					}
-				}else{
-					var t;
-					var tdesc;
-					for(var i=0;i<seleccion.length;i++){
-						for(var j=0;j<clipboard.length;j++){
-							t=clipboard[j].cloneNode(true);
+		//d
+		// case 68:me=document.getElementById("interfazDeEdicion");
+				// var r=new Array();
+				// alert(me.children.length);
+				// for(var i=0;i<me.children.length;i++) descendents(me.children[i],r)
+				// for(var i=0;i<r.length;i++) alert(r[i]);
+				// break;
+		//Ctrl+V
+		case 86:if(e.ctrlKey==true){
+					if(seleccion.length==0){
+						var iDE=document.getElementById("interfazDeEdicion");
+						var t;
+						var tdesc;
+						for(var i=0;i<clipboard.length;i++){
+							t=clipboard[i].cloneNode(true);
 							addEvents(t);
 							t.classList.remove("seleccionado");
 							tdesc=t.querySelectorAll("*");
@@ -294,19 +329,36 @@ function handleKeyDown(e){
 								addEvents(obj);
 								obj.classList.remove("seleccionado");
 							});
-							seleccion[i].appendChild(t);
+							iDE.appendChild(t);
+						}
+					}else{
+						var t;
+						var tdesc;
+						for(var i=0;i<seleccion.length;i++){
+							for(var j=0;j<clipboard.length;j++){
+								t=clipboard[j].cloneNode(true);
+								addEvents(t);
+								t.classList.remove("seleccionado");
+								tdesc=t.querySelectorAll("*");
+								[].forEach.call(tdesc,function(obj){
+									addEvents(obj);
+									obj.classList.remove("seleccionado");
+								});
+								seleccion[i].appendChild(t);
+							}
 						}
 					}
 				}
 				break;
-		//X
-		case 88:clipboard=new Array;
-				while(seleccion.length>0){
-					if(seleccion[0]==null||seleccion[0].parentNode==null){
-						seleccion.shift()
-					}else{
-						clipboard.push(seleccion[0].cloneNode(true));
-						seleccion[0].parentNode.removeChild(seleccion[0]);
+		//Ctrl+X
+		case 88:if(e.ctrlKey==true){
+					clipboard=new Array;
+					while(seleccion.length>0){
+						if(seleccion[0]==null||seleccion[0].parentNode==null) seleccion.shift();
+						else{
+							clipboard.push(seleccion[0].cloneNode(true));
+							seleccion[0].parentNode.removeChild(seleccion[0]);
+						}
 					}
 				}
 				break;
@@ -315,21 +367,25 @@ function handleKeyDown(e){
 
 ////*******Miscelaneous*******////
 function insertAfter(node,reference){ 
-	if(reference.nextSibling){ 
-		reference.parentNode.insertBefore(node,reference.nextSibling); 
-	} else { 
-		reference.parentNode.appendChild(node); 
-	}
+	if(reference.nextSibling) reference.parentNode.insertBefore(node,reference.nextSibling); 
+	else reference.parentNode.appendChild(node);
 }
 
 function addEvents(element){
+		element.addEventListener('dblclick', handleDoubleClick, false);
 		element.addEventListener('dragstart', handleDragStartForResize, false);
 		element.addEventListener('dragenter', handleDragEnter, false);
 		element.addEventListener('dragover', handleDragOver, false);
 		element.addEventListener('dragleave', handleDragLeave, false);
 		element.addEventListener('drop', handleDropForMove, false);
 		element.addEventListener('dragend', handleDragEndForResize, false);
-		element.addEventListener('mousedown', handleMouseDown, false);
+		element.addEventListener('click', handleClick, false);
+		element.addEventListener('mousemove', handleMouseMove, false);
 		element.draggable = true;
 }
 
+
+function descendents(node,des){
+	des.push(node)
+	for(var i=0;i<node.children.length;i++) descendents(node.children[i],des)
+}
