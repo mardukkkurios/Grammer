017df981a53b7b8afcb6e3a363dc688f5587ba98
KaizenCris <crisal.h.m@gmail.com>
2013-12-02 21:57:03 -0600

    Selector de clases y sus metodos implementados
    Cosas siguientes por hacer trabajar en la parte de seleccion de ids. Una
    vez que lic selecciona una correguir mis styles a ese id.
    Luego de eso generador de codigo CSS

---
+++ js/constructorDeSeleccionDeClases.js
@@ -0,0 +1,99 @@
+//Inicializacion
+$(document).ready(function(){
+	$(".SelectorDeClases .izquierdaAbajo").click(function(){
+	agregaNuevaClaseALasTagsDeClases($(this).prev(), "", true);
+	});
+	//construirSelectorDeClases(".hola, .adios, .lalala");
+	//construirSelectorDeClases(".hola, .adios, .lalala");
+	//alert(getSelectorDeClasesValue());
+});
+function agregaNuevaClaseALasTagsDeClases(container, cad, selected){
+	container.append(
+		'<div>'+
+			'<a class="textoDeLaClase"></a>'+
+			'<input/>'+
+			'<a href="#" class="eliminarEstaClaseMolesta">x</a>'+
+		'</div>');
+	var nuevo = container.children().last().children();
+	nuevo.first().click(desapareceTextoDeClaseApareceInput)
+					.text(cad);
+	nuevo.first().next().blur(apareceTextDeClaseDesapareceInput)
+						.bind('keypress', prevenirTeclasPresionadasAlAgregarUnaClase);
+	nuevo.last().click(eliminaEstaClaseMolesta);
+	if(!isBlank(cad) && selected == false)
+		return;
+	nuevo.first()[0].click();
+	nuevo.first().next()[0].focus();
+}
+function prevenirTeclasPresionadasAlAgregarUnaClase(event) {
+	var regex = new RegExp("[,; ]");
+	var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+	if (regex.test(key)) {
+			event.preventDefault();		
+		}else{
+			if(event.which === 13){
+				$(this).blur();
+				return
+			}
+		}
+}
+function eliminaEstaClaseMolesta(){ 
+	$(this).parent().remove();
+}
+function apareceTextDeClaseDesapareceInput(){//ocurre en blur del input
+	if(isBlank($(this).val())){
+		//destrullelo
+		$(this).parent().remove();
+		return;
+	}
+	event.preventDefault();
+	//modificalo
+	var cad = $(this).val();
+	if(cad[0]!='.') cad = "."+cad;
+	$(this).prev().css('display', 'inline')
+				.text(cad);
+	$(this).css('display', 'none');
+	$(this).next().css('display', 'inline');
+}
+function desapareceTextoDeClaseApareceInput(){ //ocurre en clic agegar
+	$(this).css('display', 'none');
+	$(this).next().css('display', 'inline')
+				.val($(this).text())
+				.focus()
+				.select();
+	$(this).next().next().css('display', 'none');
+}
+
+function getSelectorDeClasesValue(){
+	var result = "";
+	var clases = $(".objectClassesContainer").children().find("a.textoDeLaClase");
+	clases.each(function() {
+		result += $(this).text()+", ";
+	});
+	return result.substring(0, result.length-2);
+}
+function corrigeClaseYBorraEspacios( cad ){
+	var cad2 = "";
+	var i = 0;
+	var len = cad.length;
+	while(i<len){
+		if(cad[i]!=' ' && cad[i]!=','&& cad[i]!='.') cad2 += cad[i];
+		i++;
+	}
+	if(isBlank(cad2))return "";
+	return "."+cad2;
+}
+function construirSelectorDeClases(clases){
+	destruirSelectorDeClases();
+	var comaAnterior = 0;
+	var container = $(".objectClassesContainer");
+	while((coma = clases.indexOf(",", comaAnterior+1)) != -1){
+		agregaNuevaClaseALasTagsDeClases(container, corrigeClaseYBorraEspacios(clases.substring(comaAnterior+1, coma)), false);
+		comaAnterior = coma;
+	}
+	if(coma<clases.length) 
+		agregaNuevaClaseALasTagsDeClases(container, corrigeClaseYBorraEspacios(clases.substring(comaAnterior+1, clases.length)), false);
+}
+function destruirSelectorDeClases(){
+	$(".objectClassesContainer")[0].innerHTML = "";
+}
